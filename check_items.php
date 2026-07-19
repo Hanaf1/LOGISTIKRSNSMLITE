8c6e9e9856eb1a8037b7c62270b798c633fd2864
Muhammad Nor Hanafi <hanafikds825@gmail.com>
2026-07-20 01:56:15 +0700

    chore: hapus file debug dan script sementara

--- check_items.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'systems/Config.php';
-$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-$stmt = $pdo->query("SELECT COUNT(*) FROM rsns_custom_logistik_non_medis_master_barang WHERE status='Aktif'");
-echo $stmt->fetchColumn();
